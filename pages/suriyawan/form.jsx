4af59e1ff0a49d2d5dd62d6ba18dfc4d9ce3dc74
ksoni919343-lgtm <ksoni919343@gmail.com>
2025-09-08 19:31:11 +0530

    Create form.jsx

---
+++ pages/suriyawan/form.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import GoogleForm from '../../components/GoogleForm';
+
+const FormPage = () => {
+  return <GoogleForm formId="YOUR_FORM_ID" />;
+};
+
+export default FormPage;
